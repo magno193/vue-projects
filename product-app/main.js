215510aa7f0d156845030ef7f21f7c905e610b09
Alexandre Ferreira <alexmlf93@gmail.com>
2021-04-01 06:39:42 -0300

    Using list rendering with v-for

--- product-app/main.js
+++ product-app/main.js
@@ -6,5 +6,7 @@ const app = new Vue({
     imageLink: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
     imageAlt: 'Green socks',
     inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [{ id: 2234, color: 'green' }, { id: 2235, color: 'blue' }]
   }
 });
