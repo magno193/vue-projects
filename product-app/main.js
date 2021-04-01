e6d51e7bf122387e39d7ddd67942253573e54c56
Alexandre Ferreira <alexmlf93@gmail.com>
2021-04-01 07:51:54 -0300

    Using events with v-on

--- product-app/main.js
+++ product-app/main.js
@@ -7,6 +7,33 @@ const app = new Vue({
     imageAlt: 'Green socks',
     inStock: true,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-    variants: [{ id: 2234, color: 'green' }, { id: 2235, color: 'blue' }]
+    variants: [
+      {
+        id: 2234,
+        color: 'green',
+        image: './assets/vmSocks-green-onWhite.jpg',
+        imageLink: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+      },
+      {
+        id: 2235,
+        color: 'blue',
+        image: './assets/vmSocks-blue-onWhite.jpg',
+        imageLink: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+      }],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1
+      }
+    },
+    updateProduct({ image, imageLink }) {
+      this.image = image;
+      this.imageLink = imageLink;
+    }
   }
 });
