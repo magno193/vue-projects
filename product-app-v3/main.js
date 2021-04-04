9be76031a40175ad74553749c83b35db923035b5
Alexandre Ferreira <alexmlf93@gmail.com>
2021-04-04 17:59:08 -0300

    Project with vue bindings, events, data

--- product-app-v3/main.js
+++ product-app-v3/main.js
@@ -1,1 +1,27 @@
-const product = 'Socks';
+const app = Vue.createApp({
+  data() {
+    return {
+      cart: 0,
+      product: 'Socks',
+      image: './assets/images/socks_green.jpg',
+      inStock: false,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 01, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 02, color: 'blue', image: './assets/images/socks_blue.jpg' }
+      ]
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart++;
+    },
+    removeFromCart() {
+      if (this.cart > 0)
+        this.cart--;
+    },
+    updateImage(image) {
+      this.image = image;
+    }
+  },
+})
