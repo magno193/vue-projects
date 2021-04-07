0f56a3b26c3817e95ae19f95f1381c559ed48967
Alexandre Ferreira <alexmlf93@gmail.com>
2021-04-07 17:34:46 -0300

    Code splitting, lazy load routes

--- travel-app/src/views/Home.vue
+++ travel-app/src/views/Home.vue
@@ -7,7 +7,7 @@
           <h2>{{ destination.name }}</h2>
         </router-link>
         <figure>
-          <router-link :to="destination.name">
+          <router-link :to="destination.slug">
             <img
               :src="require(`@/assets/${destination.image}`)"
               :alt="destination.name"
