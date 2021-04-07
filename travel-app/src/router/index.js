37d7aa2e5197892375779336c419f8c8d594bd92
Alexandre Ferreira <alexmlf93@gmail.com>
2021-04-07 13:39:33 -0300

    Travel app initial clean project

---
+++ travel-app/src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Brazil from "../views/Brazil.vue";
+import Hawaii from "../views/Hawaii.vue";
+import Jamaica from "../views/Jamaica.vue";
+import Panama from "../views/Panama.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/brazil",
+    name: "brazil",
+    component: Brazil,
+  },
+  {
+    path: "/hawaii",
+    name: "hawaii",
+    component: Hawaii,
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: Jamaica,
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: Panama,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
