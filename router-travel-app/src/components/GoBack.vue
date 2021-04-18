0a0a5db814a98b44bb94fe0ea3a6eca0430e81d5
Alexandre Ferreira <alexmlf93@gmail.com>
2021-04-18 17:03:07 -0300

    Rename folder

---
+++ router-travel-app/src/components/GoBack.vue
@@ -0,0 +1,24 @@
+<template>
+  <span class="go-back">
+    <button @click="goBack">go back</button>
+  </span>
+</template>
+<script>
+export default {
+  name: "GoBack",
+  methods: {
+    goBack() {
+      return this.$router.go(-1);
+    },
+  },
+};
+</script>
+<style scoped>
+.go-back {
+  display: flex;
+}
+button {
+  border: 0;
+  cursor: pointer;
+}
+</style>
---
+++ router-travel-app/src/views/DestinationDetails.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <GoBack />
+    <section class="destination">
+      <h1>{{ destination.name }}</h1>
+      <div class="destination-details">
+        <img
+          :src="require(`@/assets/${destination.image}`)"
+          :alt="destination.name"
+        />
+        <p>{{ destination.description }}</p>
+      </div>
+    </section>
+    <section class="experiences">
+      <h2>Top experiences in: {{ destination.name }}</h2>
+      <div class="cards">
+        <div
+          class="card"
+          v-for="experience in destination.experiences"
+          :key="experience.slug"
+        >
+          <router-link
+            :to="{
+              name: 'experienceDetails',
+              params: { experienceSlug: experience.slug },
+            }"
+          >
+            <img
+              :src="require(`@/assets/${experience.image}`)"
+              :alt="experience.name"
+            />
+            <span class="card__text">
+              {{ experience.name }}
+            </span>
+          </router-link>
+        </div>
+      </div>
+      <router-view :key="$route.path" />
+    </section>
+  </div>
+</template>
+
+<script>
+import store from "@/store.js";
+import GoBack from "@/components/GoBack.vue";
+export default {
+  name: "DestinationDetails",
+  components: { GoBack },
+  props: {
+    slug: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    destination() {
+      return store.destinations.find(
+        (destination) => destination.slug == this.slug
+      );
+    },
+  },
+};
+</script>
+
+<style scoped>
+img {
+  min-width: 400px;
+  height: 100%;
+}
+.destination-details {
+  display: flex;
+  justify-content: space-between;
+}
+p {
+  margin: 0 40px;
+  font-size: 20px;
+  text-align: left;
+}
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.cards img {
+  max-height: 200px;
+}
+.card {
+  padding: 0 20px;
+  position: relative;
+  margin: 0 auto;
+}
+.card__text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 25px;
+  font-weight: bold;
+  text-decoration: none;
+}
+@media (max-width: 900px) {
+  .destination-details {
+    flex-wrap: wrap;
+  }
+  img {
+    margin: 0 auto;
+  }
+  p {
+    margin: 40px 20px;
+  }
+}
+</style>
