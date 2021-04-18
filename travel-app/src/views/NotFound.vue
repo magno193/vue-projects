690a50f5b305bc52c3234097795b317adf9b1ac5
Alexandre Ferreira <alexmlf93@gmail.com>
2021-04-18 17:04:59 -0300

    Rmv

--- travel-app/src/views/NotFound.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>Not Found</h1>
-    <p>
-      We couldn't find that page. Try going to
-      <router-link :to="{ name: 'home' }">Home</router-link>
-    </p>
-  </div>
-</template>
